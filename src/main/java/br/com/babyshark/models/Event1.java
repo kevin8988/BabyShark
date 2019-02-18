337f1e9b8a6326cc0b354ceb4c02fec18b9f4ae6
KevinChips <kevsilva07@gmail.com>
2019-02-18 15:11:02 -0300

    Adicionado a classe de modelo que representa um evento

--- src/main/java/br/com/babyshark/models/Event1.java
+++ src/main/java/br/com/babyshark/models/Event1.java
@@ -17,7 +17,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-public class Event implements Serializable {
+public class Event1 implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
