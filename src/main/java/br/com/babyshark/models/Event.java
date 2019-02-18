c0d6a2c10f57fb504b4b0b042f7fb995b99aad63
KevinChips <kevsilva07@gmail.com>
2019-02-18 15:06:29 -0300

    Adicionado a classe de modelo que representa um evento

---
+++ src/main/java/br/com/babyshark/models/Event.java
@@ -0,0 +1,126 @@
+package br.com.babyshark.models;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Event implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIME)
+	private Date initialHour;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIME)
+	private Date endHour;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Calendar dayOfEvent;
+
+	@ManyToOne
+	private User user;
+
+	@ManyToOne
+	private ONG ong;
+
+	@ManyToMany
+	private Set<User> participants = new HashSet<User>();
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getInitialHour() {
+		return initialHour;
+	}
+
+	public void setInitialHour(Date initialHour) {
+		this.initialHour = initialHour;
+	}
+
+	public Date getEndHour() {
+		return endHour;
+	}
+
+	public void setEndHour(Date endHour) {
+		this.endHour = endHour;
+	}
+
+	public Calendar getDayOfEvent() {
+		return dayOfEvent;
+	}
+
+	public void setDayOfEvent(Calendar dayOfEvent) {
+		this.dayOfEvent = dayOfEvent;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public ONG getOng() {
+		return ong;
+	}
+
+	public void setOng(ONG ong) {
+		this.ong = ong;
+	}
+
+	public Set<User> getParticipants() {
+		return participants;
+	}
+
+	public void setParticipants(Set<User> participants) {
+		this.participants = participants;
+	}
+
+}
