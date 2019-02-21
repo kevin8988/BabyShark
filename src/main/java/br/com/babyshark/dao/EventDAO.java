57526cf9662a716b7392f99cbbbbb583296ad2bd
KevinChips <kevsilva07@gmail.com>
2019-02-21 11:06:46 -0300

    Adicionado método de busca de todos os eventos, de busca por título na interface EventDAO

--- src/main/java/br/com/babyshark/dao/EventDAO.java
+++ src/main/java/br/com/babyshark/dao/EventDAO.java
@@ -1,5 +1,15 @@
 package br.com.babyshark.dao;
 
+import java.util.List;
+
+import br.com.babyshark.models.Event;
+
 public interface EventDAO {
+	
+	public List<Event> getAllEvents();
+	
+	public List<Event> getEventsByTitle(String title);
+	
+	
 
 }
