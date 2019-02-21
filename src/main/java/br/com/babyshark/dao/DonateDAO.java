09df9f55c341e821237308984b283c816b330e75
KevinChips <kevsilva07@gmail.com>
2019-02-21 10:34:36 -0300

    Adicionado alguns métodos na interface DonateDAO

--- src/main/java/br/com/babyshark/dao/DonateDAO.java
+++ src/main/java/br/com/babyshark/dao/DonateDAO.java
@@ -1,5 +1,19 @@
 package br.com.babyshark.dao;
 
+import java.util.List;
+
+import br.com.babyshark.models.Category;
+import br.com.babyshark.models.Donate;
+import br.com.babyshark.models.User;
+
 public interface DonateDAO {
+	
+	public List<Donate> getAllDonates();
+	
+	public List<Donate> getDonatesByUser(User user);
+	
+	public List<Donate> getDonatesDonated();
+	
+	public List<Donate> getDonatesByCategories(Category... categories);
 
 }
