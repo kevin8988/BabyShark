4b3a169d9008f791b865d242a056a300342e65f4
KevinChips <kevsilva07@gmail.com>
2019-02-22 14:53:12 -0300

    Adicionado o método que retorna eventos por usuario e por ong na interface EventDAO

--- src/main/java/br/com/babyshark/dao/EventDAO.java
+++ src/main/java/br/com/babyshark/dao/EventDAO.java
@@ -3,12 +3,18 @@ package br.com.babyshark.dao;
 import java.util.List;
 
 import br.com.babyshark.models.Event;
+import br.com.babyshark.models.ONG;
+import br.com.babyshark.models.User;
 
 public interface EventDAO {
 	
 	public List<Event> getAllEvents();
 	
 	public List<Event> getEventsByTitle(String title);
+	
+	public List<Event> getEventsByUser(User user);
+	
+	public List<Event> getEventsByONG(ONG ong);
 	
 
 }
