10a17bf31e436bf383fde77e12f7a20867434a9f
KevinChips <kevsilva07@gmail.com>
2019-02-22 14:39:14 -0300

    Adicionado o método que retorna todas as categorias na interface CategoryDAO

--- src/main/java/br/com/babyshark/dao/CategoryDAO.java
+++ src/main/java/br/com/babyshark/dao/CategoryDAO.java
@@ -1,5 +1,11 @@
 package br.com.babyshark.dao;
 
+import java.util.List;
+
+import br.com.babyshark.models.Category;
+
 public interface CategoryDAO {
+	
+	public List<Category> getAllCategories();
 
 }
