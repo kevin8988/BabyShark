7980ce0cd1b3a6165e9f8b199f46d41745820d75
KevinChips <kevsilva07@gmail.com>
2019-02-27 19:59:12 -0300

    Criação da classe user detail que representa detalhes do user, e do enum gender

---
+++ src/main/java/br/com/babyshark/models/UserDetail.java
@@ -0,0 +1,81 @@
+package br.com.babyshark.models;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "user_detail")
+public class UserDetail implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "day_of_birth")
+	@Temporal(TemporalType.DATE)
+	private Calendar dayOfBirth;
+
+	@Column
+	private String fone;
+
+	@Enumerated(EnumType.STRING)
+	@Column
+	private Gender gender;
+
+	@OneToOne(mappedBy = "userDetail")
+	private User user;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Calendar getDayOfBirth() {
+		return dayOfBirth;
+	}
+
+	public void setDayOfBirth(Calendar dayOfBirth) {
+		this.dayOfBirth = dayOfBirth;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public String getFone() {
+		return fone;
+	}
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
