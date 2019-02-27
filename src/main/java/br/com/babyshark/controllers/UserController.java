6de45ecfce56aa602858b9676a3061f7c9851ea3
KevinChips <kevsilva07@gmail.com>
2019-02-27 20:12:53 -0300

    Adicionado o método que recebe o cadastro

--- src/main/java/br/com/babyshark/controllers/UserController.java
+++ src/main/java/br/com/babyshark/controllers/UserController.java
@@ -1,17 +1,28 @@
 package br.com.babyshark.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.babyshark.models.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
 	@RequestMapping("/register")
-	public String register() {
+	public String register(Model model) {
+		model.addAttribute("user", new User());
 		return "user/register";
 	}
-	
+
+	@RequestMapping("/registerProcess")
+	public String registerProcess(@ModelAttribute("user") User user) {
+		System.out.println(user);
+		return "home";
+	}
+
 	@RequestMapping("/login")
 	public String login() {
 		return "user/login";
