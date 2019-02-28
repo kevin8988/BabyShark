812386bf3e2f9e155c9ed87c7fd26947d73f17d8
KevinChips <kevsilva07@gmail.com>
2019-02-28 08:34:45 -0300

    Adicionado métodos nas classes de DAO

--- src/main/java/br/com/babyshark/dao/DonateDAO.java
+++ src/main/java/br/com/babyshark/dao/DonateDAO.java
@@ -2,20 +2,23 @@ package br.com.babyshark.dao;
 
 import java.util.List;
 
+import br.com.babyshark.models.Address;
 import br.com.babyshark.models.Category;
 import br.com.babyshark.models.Donate;
 import br.com.babyshark.models.User;
 
 public interface DonateDAO {
-	
+
 	public List<Donate> getAllDonates();
-	
+
 	public List<Donate> getDonatesByUser(User user);
-	
+
 	public List<Donate> getDonatesDonated();
-	
+
 	public List<Donate> getDonatesByCategories(Category... categories);
-	 
+
 	public List<Donate> getDonatesByName(String name);
+
+	public List<Donate> getDonatesByAddress(Address adress);
 
 }
--- src/main/java/br/com/babyshark/dao/UserDAO.java
+++ src/main/java/br/com/babyshark/dao/UserDAO.java
@@ -1,5 +1,10 @@
 package br.com.babyshark.dao;
 
-public interface UserDAO {
+import br.com.babyshark.models.User;
 
+public interface UserDAO {
+	
+	public User getUserById(Integer id);
+	
+	
 }
