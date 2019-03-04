17a41a42c3251e207d8ef88b6cbe0fac722d434e
KevinChips <kevsilva07@gmail.com>
2019-03-04 18:48:02 -0300

    criação da implentação do userdao

---
+++ src/main/java/br/com/babyshark/dao/UserDAOImpl.java
@@ -0,0 +1,30 @@
+package br.com.babyshark.dao;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.babyshark.models.User;
+
+@Repository
+@Transactional
+public class UserDAOImpl implements UserDAO {
+
+	@Autowired
+	private EntityManager em;
+
+	public User getUserById(Integer id) {
+		return null;
+	}
+
+	public void insert(User user) {
+		if (user.getId() != null) {
+			em.persist(user);
+		} else {
+			em.merge(user);
+		}
+	}
+
+}
