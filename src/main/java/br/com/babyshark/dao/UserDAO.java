9573a4d294bfe27f53a9d83373e80a38bad479ca
KevinChips <kevsilva07@gmail.com>
2019-03-04 15:36:54 -0300

    adicionado método de insert no userdao

--- src/main/java/br/com/babyshark/dao/UserDAO.java
+++ src/main/java/br/com/babyshark/dao/UserDAO.java
@@ -3,8 +3,9 @@ package br.com.babyshark.dao;
 import br.com.babyshark.models.User;
 
 public interface UserDAO {
-	
+
 	public User getUserById(Integer id);
-	
-	
+
+	public void insert(User user);
+
 }
