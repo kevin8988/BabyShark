f50d057ec03d9ecbe6d80cf09409971ba390d967
KevinChips <kevsilva07@gmail.com>
2019-03-06 15:42:48 -0300

    adicionado atributo e implementação do método insert

--- src/main/java/br/com/babyshark/service/UserServiceImpl.java
+++ src/main/java/br/com/babyshark/service/UserServiceImpl.java
@@ -1,19 +1,25 @@
 package br.com.babyshark.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.babyshark.dao.UserDAO;
 import br.com.babyshark.entity.User;
 
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserDAO userDAO;
 
 	public User getUserById(Integer id) {
 		return null;
 	}
 
 	public void insert(User user) {
+		userDAO.insert(user);
 	}
 
 	public User getUserByEmail(String email) {
