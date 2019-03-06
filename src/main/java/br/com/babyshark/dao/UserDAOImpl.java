eb2584ad9b73c89294e040ab847b3083f84b97b4
KevinChips <kevsilva07@gmail.com>
2019-03-06 15:41:54 -0300

    fix imports

--- src/main/java/br/com/babyshark/dao/UserDAOImpl.java
+++ src/main/java/br/com/babyshark/dao/UserDAOImpl.java
@@ -5,7 +5,6 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import br.com.babyshark.entity.User;
 
