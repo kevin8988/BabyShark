7d032a2b31e04e38d392a2c2d3a4591d5b5ab66d
KevinChips <kevsilva07@gmail.com>
2019-03-13 15:18:20 -0300

    criação da interface de service de donates

---
+++ src/main/java/br/com/babyshark/service/DonateService.java
@@ -0,0 +1,11 @@
+package br.com.babyshark.service;
+
+import java.util.List;
+
+import br.com.babyshark.entity.Donate;
+
+public interface DonateService {
+	
+	public List<Donate> getAllDonates();	
+
+}
