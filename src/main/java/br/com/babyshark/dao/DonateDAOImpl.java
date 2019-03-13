fff63e7fb27e62a50425e4ce9a3bbe407c8da0ab
KevinChips <kevsilva07@gmail.com>
2019-03-13 15:22:11 -0300

    implementação do metodo getAllDonates

--- src/main/java/br/com/babyshark/dao/DonateDAOImpl.java
+++ src/main/java/br/com/babyshark/dao/DonateDAOImpl.java
@@ -2,15 +2,24 @@ package br.com.babyshark.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
 import br.com.babyshark.entity.Address;
 import br.com.babyshark.entity.Category;
 import br.com.babyshark.entity.Donate;
 import br.com.babyshark.entity.User;
 
+@Repository
 public class DonateDAOImpl implements DonateDAO {
 
+	@PersistenceContext
+	private EntityManager em;
+
 	public List<Donate> getAllDonates() {
-		return null;
+		return em.createQuery("from Donate", Donate.class).getResultList();
 	}
 
 	public List<Donate> getDonatesByUser(User user) {
