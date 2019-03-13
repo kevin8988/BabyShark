904af937fc0dd773309f325dfcb5e3cfc18190b2
KevinChips <kevsilva07@gmail.com>
2019-03-13 15:20:01 -0300

    criação da classe de impl de dao de donates

---
+++ src/main/java/br/com/babyshark/dao/DonateDAOImpl.java
@@ -0,0 +1,36 @@
+package br.com.babyshark.dao;
+
+import java.util.List;
+
+import br.com.babyshark.entity.Address;
+import br.com.babyshark.entity.Category;
+import br.com.babyshark.entity.Donate;
+import br.com.babyshark.entity.User;
+
+public class DonateDAOImpl implements DonateDAO {
+
+	public List<Donate> getAllDonates() {
+		return null;
+	}
+
+	public List<Donate> getDonatesByUser(User user) {
+		return null;
+	}
+
+	public List<Donate> getDonatesDonated() {
+		return null;
+	}
+
+	public List<Donate> getDonatesByCategories(Category... categories) {
+		return null;
+	}
+
+	public List<Donate> getDonatesByName(String name) {
+		return null;
+	}
+
+	public List<Donate> getDonatesByAddress(Address adress) {
+		return null;
+	}
+
+}
--- src/main/java/br/com/babyshark/service/DonateServiceImpl.java
+++ src/main/java/br/com/babyshark/service/DonateServiceImpl.java
@@ -1,5 +1,14 @@
 package br.com.babyshark.service;
 
-public class DonateServiceImpl {
+import java.util.List;
+
+import br.com.babyshark.entity.Donate;
+
+public class DonateServiceImpl implements DonateService{
+	
+	public List<Donate> getAllDonates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
