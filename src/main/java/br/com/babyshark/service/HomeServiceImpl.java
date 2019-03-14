27518a41b042768a7b0ebb2d7e621e12a2e9c6f1
KevinChips <kevsilva07@gmail.com>
2019-03-13 21:15:06 -0300

    implementação dos métodos

--- src/main/java/br/com/babyshark/service/HomeServiceImpl.java
+++ src/main/java/br/com/babyshark/service/HomeServiceImpl.java
@@ -2,23 +2,32 @@ package br.com.babyshark.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.babyshark.dao.DonateDAO;
+import br.com.babyshark.dao.EventDAO;
 import br.com.babyshark.entity.Donate;
 import br.com.babyshark.entity.Event;
 
 @Service
 public class HomeServiceImpl implements HomeService {
 	
+	@Autowired
+	private DonateDAO donateDAO;
+	
+	@Autowired
+	private EventDAO eventDAO;
+	
 	@Transactional
 	public List<Event> getThreeNearbyEvents() {
-		return null;
+		return eventDAO.getThreeNearbyEvents();
 	}
 
 	@Transactional
 	public List<Donate> getLastThreeDonates() {
-		return null;
+		return donateDAO.getLastThreeDonates();
 	}
 
 }
