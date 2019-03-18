efcf1499547fcd329fa960cb355174f8d39fced9
KevinChips <kevsilva07@gmail.com>
2019-03-18 14:32:49 -0300

    criação do color dao

---
+++ src/main/java/br/com/babyshark/dao/ColoDAOImpl.java
@@ -0,0 +1,22 @@
+package br.com.babyshark.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.babyshark.entity.Color;
+
+@Repository
+public class ColoDAOImpl implements ColorDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public List<Color> getAllColorsDonate() {
+		return em.createQuery("select distinct c from Color c join fetch c.donates", Color.class).getResultList();
+	}
+
+}
---
+++ src/main/java/br/com/babyshark/dao/ColorDAO.java
@@ -0,0 +1,11 @@
+package br.com.babyshark.dao;
+
+import java.util.List;
+
+import br.com.babyshark.entity.Color;
+
+public interface ColorDAO {
+	
+	public List<Color> getAllColorsDonate();
+
+}
