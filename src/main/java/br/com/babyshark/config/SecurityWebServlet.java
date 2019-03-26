fa2eb9c912464131172544df67d35fcafd18781e
KevinChips <kevsilva07@gmail.com>
2019-03-26 15:26:34 -0300

    configuração do spring security

---
+++ src/main/java/br/com/babyshark/config/SecurityWebServlet.java
@@ -0,0 +1,7 @@
+package br.com.babyshark.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebServlet extends AbstractSecurityWebApplicationInitializer{
+
+}
